d3dbc9253c9308bddf6ccdb0357d48f9653fe43f
Mit Gajera <gajeramit180@gmail.com>
2026-09-07 19:23:40 +0530

    test: verify protocol initialization in LiteSVM

---
+++ programs/pulsecast/tests/initialize.rs
@@ -0,0 +1,63 @@
+use {
+    anchor_lang::{
+        prelude::Pubkey,
+        solana_program::{instruction::Instruction, system_program},
+        AccountDeserialize, InstructionData, ToAccountMetas,
+    },
+    litesvm::LiteSVM,
+    solana_keypair::Keypair,
+    solana_message::{Message, VersionedMessage},
+    solana_signer::Signer,
+    solana_transaction::versioned::VersionedTransaction,
+};
+
+#[test]
+fn initializes_the_devnet_protocol_config() {
+    let program_id = pulsecast::id();
+    let authority = Keypair::new();
+    let config = Pubkey::find_program_address(&[pulsecast::constants::CONFIG_SEED], &program_id).0;
+    let mut svm = LiteSVM::new();
+    let program = include_bytes!(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/../../target/deploy/pulsecast.so"
+    ));
+    svm.add_program(program_id, program).unwrap();
+    svm.airdrop(&authority.pubkey(), 1_000_000_000).unwrap();
+
+    let args = pulsecast::instructions::InitializeArgs {
+        usdc_mint: pulsecast::constants::DEVNET_USDC_MINT,
+        btc_usd_feed_id: [1; 32],
+        oracle_exponent: -8,
+        tee_validator: Pubkey::new_unique(),
+        entry_amount: 1_000_000,
+        fee_bps: 300,
+        max_error_bps: 50,
+    };
+    let instruction = Instruction::new_with_bytes(
+        program_id,
+        &pulsecast::instruction::Initialize { args: args.clone() }.data(),
+        pulsecast::accounts::Initialize {
+            config,
+            authority: authority.pubkey(),
+            system_program: system_program::ID,
+        }
+        .to_account_metas(None),
+    );
+    let blockhash = svm.latest_blockhash();
+    let message =
+        Message::new_with_blockhash(&[instruction], Some(&authority.pubkey()), &blockhash);
+    let transaction =
+        VersionedTransaction::try_new(VersionedMessage::Legacy(message), &[&authority]).unwrap();
+
+    svm.send_transaction(transaction).unwrap();
+
+    let account = svm.get_account(&config).unwrap();
+    let state =
+        pulsecast::state::GlobalConfig::try_deserialize(&mut account.data.as_slice()).unwrap();
+    assert_eq!(state.authority, authority.pubkey());
+    assert_eq!(state.usdc_mint, args.usdc_mint);
+    assert_eq!(state.entry_amount, args.entry_amount);
+    assert_eq!(state.fee_bps, args.fee_bps);
+    assert_eq!(state.max_error_bps, args.max_error_bps);
+    assert!(!state.paused);
+}
