b16a008059ab6bdac818764109f97cd47e943ec7
Mit Gajera <gajeramit180@gmail.com>
2026-09-08 12:26:58 +0530

    test: verify market lock transition

--- programs/pulsecast/tests/initialize.rs
+++ programs/pulsecast/tests/initialize.rs
@@ -12,7 +12,7 @@ use {
 };
 
 #[test]
-fn initializes_config_and_creates_a_minute_aligned_market() {
+fn initializes_market_and_locks_it_at_the_betting_deadline() {
     let program_id = pulsecast::id();
     let authority = Keypair::new();
     let config = Pubkey::find_program_address(&[pulsecast::constants::CONFIG_SEED], &program_id).0;
@@ -96,4 +96,24 @@ fn initializes_config_and_creates_a_minute_aligned_market() {
     assert_eq!(state.resolve_at, open_at + 60);
     assert_eq!(state.entry_amount, args.entry_amount);
     assert_eq!(state.status, pulsecast::state::RoundStatus::Scheduled);
+
+    let mut clock = svm.get_sysvar::<anchor_lang::prelude::Clock>();
+    clock.unix_timestamp = state.lock_at;
+    svm.set_sysvar(&clock);
+    let instruction = Instruction::new_with_bytes(
+        program_id,
+        &pulsecast::instruction::LockMarket {}.data(),
+        pulsecast::accounts::LockMarket { round }.to_account_metas(None),
+    );
+    let blockhash = svm.latest_blockhash();
+    let message =
+        Message::new_with_blockhash(&[instruction], Some(&authority.pubkey()), &blockhash);
+    let transaction =
+        VersionedTransaction::try_new(VersionedMessage::Legacy(message), &[&authority]).unwrap();
+
+    svm.send_transaction(transaction).unwrap();
+
+    let account = svm.get_account(&round).unwrap();
+    let state = pulsecast::state::Round::try_deserialize(&mut account.data.as_slice()).unwrap();
+    assert_eq!(state.status, pulsecast::state::RoundStatus::Watching);
 }
